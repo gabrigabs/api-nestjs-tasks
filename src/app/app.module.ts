d1b4b450f58be6df4603ed14d23a9a52b4f4c613
Gabriel <gabrielbrodrigues1@gmail.com>
2025-02-11 19:58:45 -0300

    feat: add TasksModule to app module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule],
+  imports: [PrismaModule, AuthModule, UsersModule, TasksModule],
   controllers: [],
   providers: [],
 })
