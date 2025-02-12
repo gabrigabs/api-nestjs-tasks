7aa7358f108f7785e1fa8eef4cbf6581bc53f16f
Gabriel <gabrielbrodrigues1@gmail.com>
2025-02-12 19:52:40 -0300

    test: update test scripts for coverage and add mock data for Prisma and tasks

---
+++ test/units/mocks/tasks.mock.ts
@@ -0,0 +1,19 @@
+export const taskBodyMock = {
+  title: 'Teste',
+  description: 'Descrição',
+};
+
+export const taskMock = {
+  id: 'f24a5742-79f4-4e5a-89b7-854429ad8b4b',
+  title: 'Teste',
+  description: 'Descrição',
+  status: 'PENDENTE',
+  userId: '93ec118f-f7a9-416f-b8db-ee2e55607e28',
+};
+
+export const paginatedTasksMock = {
+  data: [taskMock],
+  page: 1,
+  totalPages: 1,
+  total: 1,
+};
---
+++ test/units/mocks/users.mock.ts
@@ -0,0 +1,8 @@
+export const signResponseMock = {
+  accessToken: 'randomJwt',
+};
+
+export const loginOrRegisterMock = {
+  email: 'teste@teste.com',
+  password: 'teste',
+};
